8e996ef01ba2494d746ee1ed14a5381cbbceba6f
Serhii Nebolsin <SNebolsin_c@spherasolutions.com>
2024-03-25 22:06:50 +0200

    fix error page btn link

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -10,18 +10,11 @@ const ErrorPage = () => (
     </p>
     
 
-    <Link to={'/'}>
-      <div className='flex justify-center items-center'>
-       
-        <button type="button" 
-                className=" my-20 text-white bg-gray-800 
+    <Link to={'/'} className='block ml-auto mr-auto text-center text-nowrap my-20 max-w-28 text-white bg-gray-800 
                 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 
                 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 
-                dark:focus:ring-gray-700 dark:border-gray-700"> 
-                Back Home
-          </button>
-      </div>
-     
+                dark:focus:ring-gray-700 dark:border-gray-700'>
+      Back Home
     </Link>
   </>
 );
